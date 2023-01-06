7eb8196b22ccff0b2462cc1d2da292e84ec8d994
Barry Wang <36621849+Barrywww@users.noreply.github.com>
2023-01-06 00:39:35 -0500

    feat(site): implementation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from "next/head"
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// @ts-ignore
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Yuchen Wang | 王雨宸</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
